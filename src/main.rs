3f8de83c9870d5a64f84ad93ef06a34c503e3a0f
Konstantin <doeppkonstantin@gmail.com>
2025-07-26 04:24:14 +0200

    set up initial project structure with CLI parsing and tree processing

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+
+
+use clap::Parser;
+use anyhow::Result;
+use ent::*;
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    execute(cli)?;
+    Ok(())
+}
+
+
+
+fn execute(cli: Cli) -> Result<()> {
+    match &cli.depth {
+        Some(depth) => {
+            if *depth <= 0 {
+                return Err(anyhow::anyhow!("Depth must be greater than 0"));
+            }
+        },
+        None => {}
+    }
+    let cwd = std::env::current_dir()?.to_string_lossy().into_owned();
+    let path = match &cli.path {
+        Some(path) => path,
+        None => {
+            &cwd
+        }
+    };
+    let tree = TreeEntry::build(&cli, &path)?;
+    print_tree_root(&tree);
+
+    match cli.export {
+        None => {},
+        Some(format) => {
+            let export_path = std::env::current_dir()?.join("ent-tree.json");
+            tree.export(export_path, format)?;
+        }
+    }
+
+    Ok(())
+}
